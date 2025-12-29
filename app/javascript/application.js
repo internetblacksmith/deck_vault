d1e87a232f96bcbffb3cbd23f9ef99397a944a7a
Paolo Fabbri <jabawack81@gmail.com>
2025-12-29 21:38:36 +0000

    chore: register new Stimulus controllers in application.js
    - Add binder, binder-card, binder-settings controllers
    - Add card-sort, collapsible, set-filter controllers

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -12,9 +12,14 @@ window.Stimulus = Stimulus
 
 // Auto-load controllers using Stimulus' built-in resolver
 const controllerDefinitions = [
+  ["binder", () => import("controllers/binder_controller")],
+  ["binder-card", () => import("controllers/binder_card_controller")],
+  ["binder-settings", () => import("controllers/binder_settings_controller")],
+  ["card-sort", () => import("controllers/card_sort_controller")],
   ["card-update", () => import("controllers/card_update_controller")],
+  ["collapsible", () => import("controllers/collapsible_controller")],
   ["collection-progress", () => import("controllers/collection_progress_controller")],
-  ["dark-mode", () => import("controllers/dark_mode_controller")],
+  ["set-filter", () => import("controllers/set_filter_controller")],
   ["set-progress", () => import("controllers/set_progress_controller")],
   ["toast-notification", () => import("controllers/toast_notification_controller")]
 ]
